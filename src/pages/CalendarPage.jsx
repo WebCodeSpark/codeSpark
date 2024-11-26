29afe33c7b973993aa6094beca0d427d55ebd741
JiiHyunn <gh8526@hanmail.net>
2024-11-27 01:53:18 +0900

    calendar

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -1,12 +1,127 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Calendar from "react-calendar";
 import 'react-calendar/dist/Calendar.css'; // 기본 스타일
+import styled from "styled-components";
 
+// Styled Components
+const StyledCalendarWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  position: relative;
+
+  .react-calendar {
+    width: 100%;
+    border: none;
+    border-radius: 0.5rem;
+    box-shadow: 4px 2px 10px 0px rgba(0, 0, 0, 0.13);
+    padding: 3% 5%;
+    background-color: white;
+  }
+
+  .react-calendar__month-view abbr {
+    color: ${(props) => props.theme.gray_1};
+  }
+
+  .react-calendar__navigation {
+    justify-content: center;
+  }
+
+  .react-calendar__navigation button {
+    font-weight: 800;
+    font-size: 1rem;
+  }
+
+  .react-calendar__navigation button:focus {
+    background-color: white;
+  }
+
+  .react-calendar__navigation button:disabled {
+    background-color: white;
+    color: ${(props) => props.theme.darkBlack};
+  }
+
+  .react-calendar__navigation__label {
+    flex-grow: 0 !important;
+  }
+
+  .react-calendar__month-view__weekdays abbr {
+    text-decoration: none;
+    font-weight: 800;
+  }
+
+  .react-calendar__month-view__weekdays__weekday--weekend abbr[title="일요일"] {
+    color: ${(props) => props.theme.red_1};
+  }
+
+  .react-calendar__tile--now {
+    background: none;
+    abbr {
+      color: ${(props) => props.theme.primary_2};
+    }
+  }
+
+  .react-calendar__year-view__months__month {
+    border-radius: 0.8rem;
+    background-color: ${(props) => props.theme.gray_5};
+    padding: 0;
+  }
+
+  .react-calendar__tile--hasActive {
+    background-color: ${(props) => props.theme.primary_2};
+    abbr {
+      color: white;
+    }
+  }
+
+  .react-calendar__tile {
+    padding: 5px 0px 18px;
+    position: relative;
+  }
+
+  .react-calendar__year-view__months__month {
+    flex: 0 0 calc(33.3333% - 10px) !important;
+    margin-inline-start: 5px !important;
+    margin-inline-end: 5px !important;
+    margin-block-end: 10px;
+    padding: 20px 6.6667px;
+    font-size: 0.9rem;
+    font-weight: 600;
+    color: ${(props) => props.theme.gray_1};
+  }
+
+  .react-calendar__tile:enabled:hover,
+  .react-calendar__tile:enabled:focus,
+  .react-calendar__tile--active {
+    background-color: ${(props) => props.theme.yellow_2};
+    border-radius: 0.3rem;
+  }
+`;
+
+const StyledDate = styled.div`
+  position: absolute;
+  right: 7%;
+  top: 6%;
+  background-color: ${(props) => props.theme.primary_3};
+  color: ${(props) => props.theme.yellow_2};
+  width: 18%;
+  min-width: fit-content;
+  height: 1.5rem;
+  text-align: center;
+  margin: 0 auto;
+  line-height: 1.6rem;
+  border-radius: 15px;
+  font-size: 0.8rem;
+  font-weight: 800;
+`;
 
 export default function CalendarPage() {
-    const [value, onChange] = useState(new Date());
+  const [value, onChange] = useState(new Date());
 
-    return (
-        <Calendar onChange={onChange} value={value} />
-    )
+  return (
+    <StyledCalendarWrapper>
+      <Calendar onChange={onChange} value={value} />
+      <StyledDate>오늘</StyledDate>
+    </StyledCalendarWrapper>
+  );
 }
