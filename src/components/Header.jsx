bdfee81bdf41ed36b4b7c1911467da453f2d4f4e
jungbee2 <126428632+jungbee2@users.noreply.github.com>
2024-12-04 23:07:49 +0900

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
 import Weather from '../pages/WeatherPage';
 
 export default function Header({ userName, onLogout }) {
-  const navigate = useNavigate();
 
-  const handleLogoutClick = () => {
-    onLogout(); // 사용자 상태 초기화
-    navigate('/main'); // 메인 페이지로 이동
-  };
   return (
     <header>
       <h1 className="centered-title">
@@ -74,6 +68,8 @@ export default function Header({ userName, onLogout }) {
 
           .horizontal-nav li {
             margin: 0; 
+            display: flex;
+            align-items: center;
           }
 
           .horizontal-nav a,
